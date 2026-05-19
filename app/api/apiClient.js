9bd67b9ece42a459170395f8be992d45db91c23b
Cartagena <abdel_123@hotmail.es>
2026-05-19 17:35:39 -0500

    Actualizar end-poinst

--- app/api/apiClient.js
+++ app/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://plengi.com/api', // Reemplaza con tu URL
+    baseURL: 'http://127.0.0.1:8000/api', // Reemplaza con tu URL
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
